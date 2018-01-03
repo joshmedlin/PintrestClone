42a7bc7a659565b7158073268e8969a9a157126c
joshmedlin <josh.medlin@scifusion360.com>
2018-01-03 14:04:15 -0500

    e
    d

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,7 +2,7 @@ class Pin < ActiveRecord::Base
 	acts_as_votable
 	belongs_to :user
 
-	has_attached_file :image, :styles => { :medium => "300x300>" }, :bucket => ENV['AWS_BUCKET'], :s3_bucket => ENV['AWS_BUCKET']
+	#has_attached_file :image, :styles => { :medium => "300x300>" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   
 end
