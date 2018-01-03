12b526be6e2296e2525ad75b3cd721938dcdf8d2
joshmedlin <josh.medlin@scifusion360.com>
2018-01-03 10:28:04 -0500

    j
    j

--- config/puma.rb
+++ config/puma.rb
@@ -1,9 +1,39 @@
-require "active_record"
+if ENV['RACK_ENV'] != 'production' || ENV['RAILS_ENV'] != 'production'
+  workers Integer(ENV['PUMA_WORKERS'] || 4)
+end
 
-cwd = File.dirname(__FILE__)+"/.."
+threads Integer(ENV['MIN_THREADS']  || 1), Integer(ENV['MAX_THREADS'] || 16)
 
-ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
+rackup      DefaultRackup
+port ENV['PORT'] || 3000
+environment ENV['RACK_ENV'] || 'development'
+preload_app!
 
-ActiveRecord::Base.establish_connection(ENV["DATABASE_URL"] || YAML.load_file("#{cwd}/config/database.yml")[ENV["RACK_ENV"]])
+on_worker_boot do
+  # worker specific setup
+  ActiveSupport.on_load(:active_record) do
+    config = ActiveRecord::Base.configurations[Rails.env] ||
+                Rails.application.config.database_configuration[Rails.env]
+    config['pool'] = ENV['MAX_THREADS'] || 16
+    ActiveRecord::Base.establish_connection(config)
+  end
+endif ENV['RACK_ENV'] != 'production' || ENV['RAILS_ENV'] != 'production'
+workers Integer(ENV['PUMA_WORKERS'] || 4)
+end
 
-ActiveRecord::Base.verify_active_connections!
+threads Integer(ENV['MIN_THREADS']  || 1), Integer(ENV['MAX_THREADS'] || 16)
+
+rackup      DefaultRackup
+port ENV['PORT'] || 3000
+environment ENV['RACK_ENV'] || 'development'
+preload_app!
+
+on_worker_boot do
+# worker specific setup
+ActiveSupport.on_load(:active_record) do
+  config = ActiveRecord::Base.configurations[Rails.env] ||
+              Rails.application.config.database_configuration[Rails.env]
+  config['pool'] = ENV['MAX_THREADS'] || 16
+  ActiveRecord::Base.establish_connection(config)
+end
+end
